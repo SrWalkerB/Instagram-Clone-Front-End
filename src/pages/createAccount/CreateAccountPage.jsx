7195b4a3c45bd75edb53fc10849e4467fca0a367
srwalkerb <srwalkerbdevs@gmail.com>
2021-03-16 11:27:52 -0300

    Implementation API for create user

--- src/pages/createAccount/CreateAccountPage.jsx
+++ src/pages/createAccount/CreateAccountPage.jsx
@@ -1,8 +1,48 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./style.css"
 
+console.clear();
 function CreateAccountPage(){
+
+    const [resultERR, SetResultERR] = useState("");
+
+    async function GetInputData(){
+
+        const full_name = document.querySelector("#input-full-name").value;
+        const username = document.querySelector("#input-username").value;
+        const email = document.querySelector("#input-email").value;
+        const password = document.querySelector("#input-password").value;
+
+        if(full_name || username || email || password !== ""){
+
+            const userCreate = await axios({
+                method: "POST",
+                baseURL: process.env.REACT_APP_API_CREATE,
+                data: {
+                    name_full: full_name,
+                    username: username,
+                    email: email,
+                    password: password
+                },
+                validateStatus: false
+            }).then(resp => {
+                return resp.data;
+            })
+
+            
+            if(userCreate.err){
+                return SetResultERR(userCreate.err);
+            }
+
+            
+
+            SetResultERR("");
+        }
+    }
+
     return (
         <div className="container-body-login">
 
@@ -11,20 +51,21 @@ function CreateAccountPage(){
                 <h3>Sign up to see photos and videos from your friends.</h3>
 
                 <div className="container-input-login">
-                    <input placeholder="Full Name" />
-                    <input placeholder="Username" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password"/>
-                    <button>Create Account</button>
+                    <input id="input-full-name" placeholder="Full Name" />
+                    <input id="input-username" placeholder="Username" />
+                    <input id="input-email" type="email" placeholder="Email" />
+                    <input id="input-password" type="password" placeholder="Password"/>
+                    <button onClick={() => GetInputData()}>Create Account</button>
                 </div>
 
+                <p id="msg-err">{resultERR}</p>
+            </div>
                 <div className="container-option-create-account">
                     <p>Have an account?</p>
                     <Link to="/">
                         Log in
                     </Link>
                 </div>
-            </div>
         </div>
     )
 }
